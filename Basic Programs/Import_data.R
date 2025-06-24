f9fa89cc8e8a188b5f26b8bd0d3efb3435f815e3
agusrajuthaliyan <agusraju43@gmail.com>
2025-06-24 10:22:05 +0530

    Add PRGMS

---
+++ Basic Programs/Import_data.R
@@ -0,0 +1,2 @@
+data <-data <- read.csv("Datasets/iris.csv")
+data
---
+++ Basic Programs/scan_demo.R
@@ -0,0 +1,5 @@
+# To Input a series of values, double enter to stop
+
+print("Enter a series of values: ")
+val <-scan(what=integer())
+val
